3de2f797af3e5f4faea26b99020a9944a9caff53
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-15 17:12:22 +0200

    build: add jest

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+// Keep this file as .cjs to be able to run without ts-node.
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node'
+};
