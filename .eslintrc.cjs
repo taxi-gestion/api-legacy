a003f52d2326a3ac01c6acebb38874a2404851cf
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-22 12:11:25 +0200

    feat: added jest eslint (#1)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 // Do not move / rename this file, it works for IDE automatic setting.
 // Lint the files included in each typescript project with common rules
 const projects = ['./tsconfig.json'];
+const tests = ['./tsconfig.spec.json'];
 
 // Lint project using its tsconfig.json.
 const lintProjects = () => {
@@ -15,7 +16,7 @@ const lintProjects = () => {
       env: {
         node: true
       },
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ['**/*.ts'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         tsconfigRootDir: __dirname,
@@ -26,7 +27,36 @@ const lintProjects = () => {
       plugins: ['@typescript-eslint'],
       rules: {
         ...require('./.eslint-rules/eslint.rules.cjs'),
-        ...require('./.eslint-rules/typescript-eslint.rules.cjs')
+        ...require('./.eslint-rules/typescript-eslint.rules.cjs'),
+        ...require('./.eslint-rules/to-review.rules.cjs')
+      }
+    }
+  ];
+};
+
+const lintTests = () => {
+  return [
+    {
+      env: {
+        'jest/globals': true
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: tests,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      files: ['src/**/*.spec.ts'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      plugins: ['jest', '@typescript-eslint'],
+      rules: {
+        ...require('./.eslint-rules/eslint.rules.cjs'),
+        ...require('./.eslint-rules/eslint.test.rules.cjs'),
+        ...require('./.eslint-rules/typescript-eslint.rules.cjs'),
+        ...require('./.eslint-rules/typescript-eslint.test.rules.cjs'),
+        ...require('./.eslint-rules/jest.rules.cjs'),
+        ...require('./.eslint-rules/to-review.test.rules.cjs')
       }
     }
   ];
@@ -34,5 +64,5 @@ const lintProjects = () => {
 
 module.exports = {
   root: true,
-  overrides: [...lintProjects()]
+  overrides: [...lintProjects(), ...lintTests()]
 };
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,3 +1,17 @@
+# 1.0.0 (2023-05-22)
+
+
+### Features
+
+* add github release to releaserc ([#15](https://github.com/codingones-github-templates/fastify-api/issues/15)) ([2270391](https://github.com/codingones-github-templates/fastify-api/commit/22703918f0c33f66931924367317100b52ddd396))
+* add renovate.json ([f9cf070](https://github.com/codingones-github-templates/fastify-api/commit/f9cf070cec1d99504ec9c45cbc530a8d53efc811))
+* added jest eslint ([#1](https://github.com/codingones-github-templates/fastify-api/issues/1)) ([89a6234](https://github.com/codingones-github-templates/fastify-api/commit/89a6234ba7e8e8d08c2cdde1214e6f9988ee928c))
+* ci/validation ([514098f](https://github.com/codingones-github-templates/fastify-api/commit/514098f565daae41cf346aabdd594a2b98e936ff))
+* clean changelog and release ([#12](https://github.com/codingones-github-templates/fastify-api/issues/12)) ([108f5f4](https://github.com/codingones-github-templates/fastify-api/commit/108f5f47a7ca5ccfdb89a5c26a9329c0197327c8))
+* esbuild esm configuration ([62e3024](https://github.com/codingones-github-templates/fastify-api/commit/62e3024949b1b7cacb7dae5a488e757c88110d66))
+* initial commit ([dc23c9d](https://github.com/codingones-github-templates/fastify-api/commit/dc23c9d73dcdfc792419ef128221afb4e414469c))
+* precommit hook for changelog and fail-fast strategy ([#14](https://github.com/codingones-github-templates/fastify-api/issues/14)) ([cc2edbc](https://github.com/codingones-github-templates/fastify-api/commit/cc2edbc084c876b0f9f6fc4abf91ecca4b26391a))
+
 # [1.2.0](https://github.com/codingones-github-templates/fastify-api/compare/v1.1.0...v1.2.0) (2023-05-17)
 
 ### Features
