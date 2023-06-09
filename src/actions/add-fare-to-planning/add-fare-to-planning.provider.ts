ced2ab5ec768993fa2af5bf383b5c416270def6e
Romain Cambonie <romain.cambonie@gmail.com>
2023-06-09 19:09:50 +0200

    feat: added isTimeISO8601 rule to FareDraftRules

--- src/actions/add-fare-to-planning/add-fare-to-planning.provider.ts
+++ src/actions/add-fare-to-planning/add-fare-to-planning.provider.ts
@@ -7,7 +7,7 @@ import { isFrenchPhoneNumber } from '../../rules/FrenchPhoneNumber.rule';
 import { isPositive } from '../../rules/Positive.rule';
 import { isRegisteredClient } from '../../rules/RegisteredClient.rule';
 import { isDateISO8601String } from '../../rules/DateISO8601.rule';
-import {isTimeISO8601String} from "../../rules/TimeISO8601.rule";
+import { isTimeISO8601String } from '../../rules/TimeISO8601.rule';
 
 /* eslint-disable @typescript-eslint/naming-convention,@typescript-eslint/typedef */
 const DriveKind = t.keyof({ 'one-way': null, outward: null, 'go-back': null });
@@ -51,7 +51,7 @@ export const FareDraftRules = t.intersection([
     //destination: isValidAddress,
     //planning: t.intersection([isDriverPlanning, isUnassigned]),
     phone: isFrenchPhoneNumber,
-    time: isTimeISO8601String,
+    time: isTimeISO8601String
   })
 ]);
 
