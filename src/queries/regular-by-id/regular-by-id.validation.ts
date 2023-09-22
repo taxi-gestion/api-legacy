9f38e717f95ed47b42b1923e18fb08a968ad00e2
Romain Cambonie <romain.cambonie@gmail.com>
2023-09-22 11:47:18 +0200

    refactor: uuid as rule because of branded type

--- src/queries/regular-by-id/regular-by-id.validation.ts
+++ src/queries/regular-by-id/regular-by-id.validation.ts
@@ -3,10 +3,10 @@ import { pipe } from 'fp-ts/lib/function';
 import { fromEither, TaskEither } from 'fp-ts/TaskEither';
 import { Entity, RegularDetails } from '../../definitions';
 import { externalTypeCheckFor, regularDetailsEntityCodec } from '../../codecs';
-import { uuidCodec } from '../../codecs/common/uuid.codec';
+import { uuidRule } from '../../rules/common/uuid.rule';
 
 export const regularByIdValidation = (transfer: unknown): TaskEither<Errors, string> =>
-  pipe(transfer, uuidCodec.decode, fromEither);
+  pipe(transfer, uuidRule.decode, fromEither);
 
 export const regularValidation = (transfer: unknown): TaskEither<Errors, Entity & RegularDetails> =>
   pipe(transfer, externalTypeCheckFor<Entity & RegularDetails>(regularDetailsEntityCodec), fromEither);
